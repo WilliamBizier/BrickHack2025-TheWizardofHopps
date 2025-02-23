d72a00f3dd3902160808faf2833ca3f1f9507ece
CollinOlkows <cmolkows@buffalo.com>
2025-02-23 03:32:37 -0500

    updated spell casting

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,8 @@ var face = 0
 
 var count = 0
 
+var fireball = false
+
 func _ready():
 	pass
 
@@ -21,31 +23,41 @@ func _process(delta):
 	
 	velocity = direction * SPEED
 	
+	if fireball and count < 120:
+		count +=1
+	elif count == 120:
+		count = 0
 	
-	if Input.is_action_just_pressed("Action"):
+	if Input.is_action_just_pressed("Action") and fireball == false:
 		print('fireball')
+		fireball = true
 		face=5
 		animated_sprite.play('fireball_cast')
 	
 	if Input.is_action_just_pressed("Left") and face != 1 and face!=5:
 		animated_sprite.play('run')
 		animated_sprite.flip_h = true
+		fireball = false
 		face = 1
 	elif Input.is_action_just_pressed("Right") and face != 2:
 		animated_sprite.play('run')
 		animated_sprite.flip_h = false
+		fireball = false
 		face = 2
 	elif Input.is_action_just_pressed("Up") and face != 3:
 		face = 3
+		fireball = false
 		pass
 	elif Input.is_action_just_pressed("Down") and face != 0:
 		animated_sprite.play('idle')
 		face = 0
+		fireball = false
 		pass	
 	
-	if velocity == Vector2.ZERO and face != 5:
+	if velocity == Vector2.ZERO and face != 5 and fireball == false or count == 120:
 		animated_sprite.play('idle')
 		face = 0
+		fireball = false
 		
 	
 func _physics_process(delta: float):
